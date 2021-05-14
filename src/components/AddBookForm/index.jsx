1ee6b85a9d9b566593c8c5e1cc4d5434d4470f8a
Vladislav Vasilev <vasilevv22@gmail.com>
2021-05-14 14:02:40 +0300

    Added simple validation for book title and author

--- src/components/AddBookForm/index.jsx
+++ src/components/AddBookForm/index.jsx
@@ -4,6 +4,7 @@ import React, { useState } from 'react';
 const Index = ({ addBook }) => {
     const emptyBookObj = { title: '', author: '', isRead: false };
     const [book, setBook] = useState(emptyBookObj);
+    const [validation, setValidation] = useState('');
 
     const handleChange = (event) => {
         setBook({ ...book, [event.target.name]: event.target.value });
@@ -11,8 +12,15 @@ const Index = ({ addBook }) => {
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        addBook(book);
-        setBook(emptyBookObj);
+
+        if (book.title.length > 1 && book.author.length > 1) {
+            setValidation('');
+            addBook(book);
+            setBook(emptyBookObj);    
+        } else {
+            setValidation('Book title or book author must be longer');
+        }
+
     }
 
     return (
@@ -22,6 +30,7 @@ const Index = ({ addBook }) => {
             {/* <label htmlFor="author">Author</label> */}
             <input id="author" name="author" type="text" placeholder="Book author" value={book.author} onChange={handleChange} />
             <input type="submit" value="Add book" />
+            {validation && <p className="validation">{validation}</p>}
         </form>
     );
 }
